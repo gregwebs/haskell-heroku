513b7b13b184268b93481ac99ee9da7e1eb10d58
Greg Weber <greg@gregweber.info>
2014-06-20 13:30:02 -0700

    add test suite

--- test.hs
+++ test.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Web.Heroku
+import Test.Hspec
 
-main = print $ show $
-  parseDatabaseUrl "postgres://db:pass@ec2-1-1-1-1.compute-1.amazonaws.com:1234/db"
+main :: IO ()
+main = hspec $
+  describe "parseDatabaseUrl" $
+    it "extracts individual items" $
+      parseDatabaseUrl "postgres://db:pass@ec2-1-1-1-1.compute-1.amazonaws.com:1234/db" `shouldBe`
+        [ ("user","db")
+        , ("password","pass")
+        , ("host","ec2-1-1-1-1.compute-1.amazonaws.com")
+        , ("port","1234")
+        , ("dbname","db")
+        ]
